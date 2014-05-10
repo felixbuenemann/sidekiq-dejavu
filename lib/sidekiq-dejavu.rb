f031e32cc358ac8eb79743712e050ac6a1561902
Felix Bünemann <buenemann@louis.info>
2014-05-10 12:47:13 +0200

    Move intiailization code
    It's now possible to load the lib without initializing it by using
    `require: false` in bundler and requiring `sidekiq/dejavu`.

--- lib/sidekiq-dejavu.rb
+++ lib/sidekiq-dejavu.rb
@@ -1,1 +1,18 @@
 require "sidekiq/dejavu"
+
+Sidekiq.configure_server do |config|
+  config.server_middleware do |chain|
+    chain.add Sidekiq::Dejavu::Middleware::Server::Scheduler
+  end
+
+  config.on(:startup) do
+    schedules = config.options.fetch(:schedule, {})
+    Sidekiq.logger.debug "Sidekiq::Dejavu: schedules: #{schedules.inspect}"
+    if schedules.empty?
+      Sidekiq.logger.warn "Sidekiq::Dejavu: No schedule found."
+    else
+      Sidekiq.logger.info "Sidekiq::Dejavu: Loading schedules #{schedules.keys.join ','}."
+      Sidekiq::Dejavu::Manager.new(schedules).reload_schedule!
+    end
+  end
+end
--- lib/sidekiq/dejavu.rb
+++ lib/sidekiq/dejavu.rb
@@ -2,19 +2,3 @@ require 'sidekiq/dejavu/version'
 require 'sidekiq/dejavu/helper'
 require 'sidekiq/dejavu/manager'
 require 'sidekiq/dejavu/middleware/server/scheduler'
-
-Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.add Sidekiq::Dejavu::Middleware::Server::Scheduler
-  end
-
-  config.on(:startup) do
-    schedules = config.options.fetch(:schedule, {})
-    if schedules.empty?
-      Sidekiq.logger.warn "Sidekiq::Dejavu: No schedule found."
-    else
-      Sidekiq.logger.info "Sidekiq::Dejavu: Loading schedules #{schedules.keys.join ','}."
-      Sidekiq::Dejavu::Manager.new(schedules).reload_schedule!
-    end
-  end
-end
