d46c4c011cccf414f627af936eced02350eb090f
Felix Bünemann <buenemann@louis.info>
2015-11-20 17:52:30 +0100

    Add tests for helper

---
+++ test/test_helper.rb
@@ -0,0 +1,45 @@
+require_relative "helper"
+
+class TestHelper < Sidekiq::Dejavu::Test
+  include Sidekiq::Dejavu::Helper
+
+  def time
+    @time ||= Time.now
+  end
+
+  def test_next_timestamp_cron
+    interval = '* * * * *'
+    expected = time.to_f + (60 - time.to_f % 60)
+    assert_equal expected, next_timestamp(interval, time)
+  end
+
+  def test_next_timestamp_numeric
+    expected = time.to_f + 10.0
+    [10, 10.0, '10', '10.0'].each do |interval|
+      assert_equal expected, next_timestamp(interval, time)
+    end
+  end
+
+  def test_next_randomized_timestamp_cron
+    interval = '* * * * *'
+    expected = time.to_f + (60 - time.to_f % 60)
+    timestamp = next_randomized_timestamp(interval, time)
+    another_timestamp = next_randomized_timestamp(interval, time)
+
+    assert_in_delta expected, timestamp, 60.0
+    refute_equal expected, timestamp
+    refute_equal timestamp, another_timestamp
+  end
+
+  def test_next_randomized_timestamp_numeric
+    expected = time.to_f + 10.0
+    [10, 10.0, '10', '10.0'].each do |interval|
+      timestamp = next_randomized_timestamp(interval, time)
+      another_timestamp = next_randomized_timestamp(interval, time)
+
+      assert_in_delta expected, timestamp, 10.0
+      refute_equal expected, timestamp
+      refute_equal timestamp, another_timestamp
+    end
+  end
+end
